f56b2f72780c7dccb5a90613c3b88e3b0c238cce
Ajit Kumar <ajit.kumar.azad@gmail.com>
2017-02-17 15:33:59 -0800

    Sample Ext JS application demonstrating PWA capabilities.

---
+++ app/store/Personnel.js
@@ -0,0 +1,33 @@
+Ext.define('ED.store.Personnel', {
+    extend: 'Ext.data.Store',
+
+    alias: 'store.personnel',
+
+    fields: [
+        'name', 'email', 'phone'
+    ],
+    autoLoad: true,
+
+    // data: { items: [
+    //     { name: 'Jean Luc', email: "jeanluc.picard@enterprise.com", phone: "555-111-1111" },
+    //     { name: 'Worf',     email: "worf.moghsson@enterprise.com",  phone: "555-222-2222" },
+    //     { name: 'Deanna',   email: "deanna.troi@enterprise.com",    phone: "555-333-3333" },
+    //     { name: 'Data',     email: "mr.data@enterprise.com",        phone: "555-444-4444" }
+    // ]},
+
+    // proxy: {
+    //     type: 'memory',
+    //     reader: {
+    //         type: 'json',
+    //         rootProperty: 'items'
+    //     }
+    // }
+    proxy: {
+        type: 'ajax',
+        url: 'data/personnel.json',
+        reader: {
+            type: 'json',
+            rootProperty: 'items'
+        }
+    }
+});
